339085de71edefc37f83890237577ec980423d95
Raaj Giri <raj.ash2493@gmail.com>
2025-10-07 13:03:37 +0530

    URL Shortner

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
 
 app.use(express.json()); // ✅ Important
